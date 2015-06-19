44650f9769fc1d4dc3afdb174f62269de33c1692
AleDH <aleduarteh@gmail.com>
2015-06-19 14:27:36 +0200

    Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -20,14 +20,14 @@ exports.index = function(req,res){
 	busqueda="%" + busqueda + "%";
 	models.Quiz.findAll({where: ["upper(pregunta) like ?",busqueda]}).then(
 	function(quizes){
-		res.render('quizes/index.ejs',{quizes:quizes});
+		res.render('quizes/index.ejs',{quizes:quizes,errors:[]});
 	}
 	).catch(function(erro) { next(error);})
 };
 
 // GET /quizes/:id
 exports.show = function (req,res){
-	res.render('quizes/show',{quiz:req.quiz});
+	res.render('quizes/show',{quiz:req.quiz,errors:[]});
    
  };
  
@@ -39,7 +39,7 @@ exports.answer = function (req,res){
 		resultado="Correcto";
 	}
 	
-	res.render('quizes/answer',{quiz:req.quiz,respuesta:resultado});
+	res.render('quizes/answer',{quiz:req.quiz,respuesta:resultado,errors:[]});
 	
 };
 
@@ -48,13 +48,23 @@ exports.new = function(req,res){
 	var quiz = models.Quiz.build(
 		{pregunta:"Pregunta", respuesta:"Respuesta"}
 		);
-		res.render("quizes/new",{quiz:quiz});
+		res.render("quizes/new",{quiz:quiz,errors:[]});
 };
 
 // POST /quizes/create
 exports.create = function(req,res){
 	var quiz = models.Quiz.build(req.body.quiz);
-	quiz.save({fields:["pregunta","respuesta"]}).then(function(){
-		res.redirect('/quizes'); 
-	})
+	quiz
+	.validate()
+	.then(
+			function(err){
+					if (err) {
+						res.render('quizes/new', {quiz:quiz,errors:err.errors});
+					}else {
+						quiz //save: guarda en DB campos pregunta y respuesta de quiz
+						.save({fields:["pregunta","respuesta"]})
+						.then(function(){res.redirect('/quizes')})
+					}
+				}
+			);
 };
